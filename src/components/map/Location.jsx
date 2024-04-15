041ca7ee216e292b65710bf4e23db0d6d186b8f6
MangwonCassie <dudnfsla1@naver.com>
2024-04-15 15:36:39 +0900

    feat: 검색어로 호텔만 검색하도록 처리

--- src/components/map/Location.jsx
+++ src/components/map/Location.jsx
@@ -27,17 +27,32 @@ const Location = () => {
     // 장소 검색 객체를 생성합니다
     var ps = new kakao.maps.services.Places(map);
 
+
+    // 키워드로 장소를 검색합니다
+    ps.keywordSearch(searchKeyword, placesSearchCB);
+
     // 카테고리로 호텔을 검색합니다
     ps.categorySearch('AD5', placesSearchCB, { useMapBounds: true });
+
 
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
     function placesSearchCB(data, status, pagination) {
       if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
         for (var i = 0; i < data.length; i++) {
-          console.log("검색된 음식점의 갯수는 " + i + "개 입니다.")
+          console.log("검색된 호텔의 갯수는 " + i + "개 입니다.")
           console.log(data[i]);
           displayMarker(data[i]);
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+
       }
     }
 
