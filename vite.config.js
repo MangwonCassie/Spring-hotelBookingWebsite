cb4c8bfa719d7ebefe0621d538570bd4f1436d22
MangwonCassie <dudnfsla1@naver.com>
2024-05-20 19:13:22 +0900

    fix: 백엔드 배포 주소 env 파일 업데이트

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const baseURL = process.env.BACKEND_URL
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    target: baseURL, // 백엔드 API의 주소로 변경
+    changeOrigin: true,
     port: 5174 // 원하는 포트 번호로 설정
   }
 
