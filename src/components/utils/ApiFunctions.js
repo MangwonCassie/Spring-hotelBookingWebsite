8425fc7b958e6a94257b927bb3f8a03a38482a89
MangwonCassie <dudnfsla1@naver.com>
2024-06-10 15:52:40 +0900

    fix: index-abcc7c21.js:46
           DELETE https://river-hotel-91f9caaa3277.herokuapp.com/api/bookings/booking/4/delete net::ERR_FAILED 해결

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -137,7 +137,7 @@ export async function getBookingByConfirmationCode(confirmationCode) {
 /* This is the function to cancel user booking */
 export async function cancelBooking(bookingId) {
 	try {
-		const result = await api.delete(import.meta.env.VITE_BACKEND_URL + `api/bookings/booking/${bookingId}/delete`)
+		const result = await api.delete(import.meta.env.VITE_BACKEND_URL + `bookings/booking/${bookingId}/delete`)
 		return result.data
 	} catch (error) {
 		throw new Error(`Error cancelling booking :${error.message}`)
